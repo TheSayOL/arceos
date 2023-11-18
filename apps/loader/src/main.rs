58ac0fcbf3549355b9abdcf896356b74efa329fe
Your Name <you@example.com>
2023-11-18 17:04:40 +0800

    step 3

--- apps/loader/src/main.rs
+++ apps/loader/src/main.rs
@@ -12,6 +12,25 @@ const PLASH_START: usize = 0x22000000;
 // 0xffff_ffc0_0000_0000
 const RUN_START: usize = 0xffff_ffc0_8010_0000;
 
+const SYS_HELLO: usize = 1;
+const SYS_PUTCHAR: usize = 2;
+
+static mut ABI_TABLE: [usize; 16] = [0; 16];
+
+fn register_abi(num: usize, handle: usize) {
+    unsafe {
+        ABI_TABLE[num] = handle;
+    }
+}
+
+fn abi_hello() {
+    println!("[ABI:Hello] Hello, Apps!");
+}
+
+fn abi_putchar(c: char) {
+    println!("[ABI:Print] {c}");
+}
+
 #[cfg_attr(feature = "axstd", no_mangle)]
 fn main() {
     let apps_start = PLASH_START as *const u8;
@@ -22,27 +41,38 @@ fn main() {
     let code = unsafe { core::slice::from_raw_parts(apps_start, apps_size) };
     println!("content: {:#x}", bytes_to_usize(&code[..8]));
 
-
     let load_size = apps_size;
-    let run_code = unsafe {
-        core::slice::from_raw_parts_mut(RUN_START as *mut u8 , load_size)
-    };
+    let run_code = unsafe { core::slice::from_raw_parts_mut(RUN_START as *mut u8, load_size) };
     run_code.copy_from_slice(code);
     println!("run code {:?}, addr = [{:?}]", run_code, run_code.as_ptr());
-    
+
     println!("Load payload ok!");
 
-    println!("execute app...");
+    register_abi(SYS_HELLO, abi_hello as usize);
+    register_abi(SYS_PUTCHAR, abi_putchar as usize);
 
+    println!("Execute app ...");
+    let arg0: u8 = b'A';
+
+    // execute app
     unsafe {
         core::arch::asm!("
-            li t2, {run_start}
-            jalr t2
-            j .", 
-            run_start = const RUN_START
-        );
+        li      t0, {abi_num}
+        slli    t0, t0, 3
+        la      t1, {abi_table}
+        add     t1, t1, t0
+        ld      t1, (t1)
+        jalr    t1
+        li      t2, {run_start}
+        jalr    t2
+        j       .",
+            run_start = const RUN_START,
+            abi_table = sym ABI_TABLE,
+            //abi_num = const SYS_HELLO,
+            abi_num = const SYS_PUTCHAR,
+            in("a0") arg0,
+        )
     }
-
 }
 
 #[inline]
