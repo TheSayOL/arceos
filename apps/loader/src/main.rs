02e80072318656a53154bd60f1913f5b8efb8155
Your Name <you@example.com>
2023-11-18 19:05:23 +0800

    step 5

--- apps/loader/src/main.rs
+++ apps/loader/src/main.rs
@@ -10,11 +10,12 @@ const PLASH_START: usize = 0x22000000;
 // app running aspace
 // SBI(0x80000000) -> App <- Kernel(0x80200000)
 // 0xffff_ffc0_0000_0000
-const RUN_START: usize = 0xffff_ffc0_8010_0000;
+// const RUN_START: usize = 0xffff_ffc0_8010_0000;
+const RUN_START: usize = 0x4010_0000;
 
+// ABI TABLE
 const SYS_HELLO: usize = 1;
 const SYS_PUTCHAR: usize = 2;
-
 static mut ABI_TABLE: [usize; 16] = [0; 16];
 
 fn register_abi(num: usize, handle: usize) {
@@ -31,8 +32,43 @@ fn abi_putchar(c: char) {
     println!("[ABI:Print] {c}");
 }
 
+//
+// App aspace
+//
+#[link_section = ".data.app_page_table"]
+static mut APP_PT_SV39: [u64; 512] = [0; 512];
+
+unsafe fn init_app_page_table() {
+    // 0x8000_0000..0xc000_0000, VRWX_GAD, 1G block
+    APP_PT_SV39[2] = (0x80000 << 10) | 0xef;
+    // 0xffff_ffc0_8000_0000..0xffff_ffc0_c000_0000, VRWX_GAD, 1G block
+    APP_PT_SV39[0x102] = (0x80000 << 10) | 0xef;
+
+    // 0x0000_0000..0x4000_0000, VRWX_GAD, 1G block
+    APP_PT_SV39[0] = (0x00000 << 10) | 0xef;
+
+    // For App aspace!
+    // 0x4000_0000..0x8000_0000, VRWX_GAD, 1G block
+    APP_PT_SV39[1] = (0x80000 << 10) | 0xef;
+}
+
+unsafe fn switch_app_aspace() {
+    use riscv::register::satp;
+    let page_table_root = APP_PT_SV39.as_ptr() as usize - axconfig::PHYS_VIRT_OFFSET;
+    satp::set(satp::Mode::Sv39, 0, page_table_root >> 12);
+    riscv::asm::sfence_vma_all();
+}
+
 #[cfg_attr(feature = "axstd", no_mangle)]
 fn main() {
+    // switch aspace from kernel to app
+    unsafe {
+        init_app_page_table();
+    }
+    unsafe {
+        switch_app_aspace();
+    }
+
     let apps_start = PLASH_START as *const u8;
     let apps_size = 32; // Dangerous!!! We need to get accurate size of apps.
 
@@ -52,17 +88,19 @@ fn main() {
     register_abi(SYS_PUTCHAR, abi_putchar as usize);
 
     println!("Execute app ...");
-    let arg0: u8 = b'A';
+    // let arg0: u8 = b'A';
 
     // execute app
-    unsafe { core::arch::asm!("
+    unsafe {
+        core::arch::asm!("
         la      a7, {abi_table}
         li      t2, {run_start}
         jalr    t2
         j       .",
-        run_start = const RUN_START,
-        abi_table = sym ABI_TABLE,
-    )}
+            run_start = const RUN_START,
+            abi_table = sym ABI_TABLE,
+        )
+    }
 }
 
 #[inline]
